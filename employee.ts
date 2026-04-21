740bab86f2f1280f50b553b9dbea2eaca7657c23
priyankapujari521-wq <priyankapujari521@gmail.com>
2026-04-21 14:51:28 +0530

    Delete employee.ts

--- employee.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-employee',
-  imports: [],
-  templateUrl: './employee.html',
-  styleUrl: './employee.css',
-})
-export class Employee {}
